b91212d8b4f5ce3710c53018288f135844c9baeb
Ferdows Ahmmed <ferdows962@gmail.com>
2023-05-19 17:30:51 +0600

    meaningfull user data about toy inserted in database

--- src/Pages/SingleToy/SingleToy.jsx
+++ src/Pages/SingleToy/SingleToy.jsx
@@ -12,16 +12,21 @@ const SingleToy = () => {
   }, []);
 
   return (
-    <div className="p-4 bg-white shadow-lg rounded-lg">
-      <img className="w-full mb-4 rounded" src={toyDetails?.pictureUrl} alt="" />
-      <h2 className="text-2xl font-bold mb-2">{toyDetails?.name}</h2>
+      <section className="mb-96">
+       <h1 className="text-center text-5xl mt-6">Toy Details</h1>
+     <div className=" p-6  m-12 bg-white shadow-lg rounded-lg">
+      <img className="w-96 mx-auto mb-4 rounded-xl" src={toyDetails?.pictureUrl} alt="" />
+     <div className="px-20 bg-white py-4">
+     <h2 className="text-2xl font-bold mb-2">{toyDetails?.name}</h2>
       <p className="text-gray-600 mb-1">Seller Name: {toyDetails?.sellerName}</p>
       <p className="text-gray-600 mb-1">Seller Email: {toyDetails?.sellerEmail}</p>
       <p className="text-gray-600">Description: {toyDetails?.description}</p>
       <p className="text-gray-600 mb-1">Available Quantity: {toyDetails?.quantity}</p>
       <p className="text-green-600 mb-1">Price: {toyDetails?.price}</p>
       <p className="text-yellow-500 mb-1">Rating: {toyDetails?.rating}</p>
+     </div>
     </div>
+   </section>
   );
 };
 
